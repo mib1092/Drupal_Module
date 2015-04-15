fabf2f7aa860c9790d73477111c62c9722b9f21c
mdwork <mazdze@gmail.com>
2015-04-15 22:47:24 +0300

    custom module

---
+++ sites/all/modules/custom/custom_template.tpl.php
@@ -0,0 +1,44 @@
+<div class="custom-block">
+    <div id="SinoptikInformer" style="width:180px;" class="SinoptikInformer type4c1">
+        <div class="siHeader"><div class="siLh">
+                <div class="siMh">
+                    <a onmousedown="siClickCount();" href="https://ua.sinoptik.ua/" target="_blank">Погода</a>
+                    <a onmousedown="siClickCount();" class="siLogo" href="https://ua.sinoptik.ua/" target="_blank"> </a>
+                </div>
+            </div>
+        </div>
+        <div class="siBody">
+            <div class="siTitle">
+                <span id="siHeader"></span>
+            </div>
+            <div class="siCity">
+                <div class="siCityName">
+                    <a onmousedown="siClickCount();" href="https://ua.sinoptik.ua/погода-<?php print $variable;?>" target="_blank">Погода у місті <span><?php print $variable; ?></span></a>
+                </div>
+                <div id="siCont0" class="siBodyContent">
+                    <div class="siLeft">
+                        <div class="siTerm"></div>
+                        <div class="siT" id="siT0"></div>
+                        <div id="weatherIco0"></div>
+                    </div>
+                    <div class="siInf">
+                        <p>вологість: <span id="vl0"></span></p><p>тиск: <span id="dav0"></span></p><p>вітер: <span id="wind0"></span></p>
+                    </div>
+                </div>
+            </div>
+            <div class="siLinks">
+                <span>
+                    <a onmousedown="siClickCount();" href="https://ua.sinoptik.ua/погода-кривий-ріг" target="_blank">Погода у Кривому Розі</a>&nbsp;
+                </span>
+                <span>
+                    <a onmousedown="siClickCount();" href="https://ua.sinoptik.ua/погода-львів" target="_blank">Погода у Львові</a>&nbsp;
+                </span>
+            </div>
+        </div>
+        <div class="siFooter">
+            <div class="siLf">
+                <div class="siMf"></div>
+            </div>
+        </div>
+    </div><script type="text/javascript" charset="UTF-8" src="//sinoptik.ua/informers_js.php?title=3&amp;wind=1&amp;cities=303028869&amp;lang=ua"></script>
+</div>
